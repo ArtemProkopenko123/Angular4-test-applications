0e5fcdf754ee8c07145046c2a0aae3d10c042483
Artem <Sundey@i.ua>
2017-09-06 19:52:40 +0300

    Artem Prokopenko

--- app/photogallery/photogallery.component.ts
+++ app/photogallery/photogallery.component.ts
@@ -11,11 +11,15 @@ export class PhotogalleryComponent implements OnInit {
     title = 'Gallery';
     newTitle = '';
     setTitle = false;
-
+    alert = false;
     public constructor(private titleService: Title ) {this.titleService.setTitle(this.title) }
 
+   
+
     onSubmit(){
-     this.titleService.setTitle(this.newTitle);   
+    if(this.setTitle) this.titleService.setTitle(this.newTitle);   
+    if(this.alert) alert(this.newTitle);
     }
+
     ngOnInit() {}
 }
